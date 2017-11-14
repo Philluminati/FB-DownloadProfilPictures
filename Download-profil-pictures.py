d4633e6c94fa0d7e82759bd330b0a3df6e7bc2a2
Philipp Hager <hagerphilipp90@gmail.com>
2017-11-14 19:08:45 +0100

    new Version

--- Download-profil-pictures.py
+++ Download-profil-pictures.py
@@ -7,53 +7,63 @@ import json
 fo = open("data.html", "r+")
 str = fo.read();
 
-int1 = str.find('InitialChatFriendsList",[],{groups:[],list:[')
-int1 += 45
+#int1 = str.find('InitialChatFriendsList",[],{groups:[],list:[')
+int1 = str.find('list:[')   # Findet die Position im Dokument wo die ID Freundesliste beginnt
+
+int1 += 6                   # Weil sonst "list:[" auch noch in der Liste dabei ist die wir einlesen
 int2 = str.find('],shortProfiles')
+
 
 
 # Calc how many char. for reading
 new = int2-int1
 
 #Read in the ID's from the html file
-fo.seek(int1, 0);
-data = fo.read(new);
+fo.seek(int1, 0);           #Spring an die Postion der Variable int1
+data = fo.read(new);        # String wo die ID's gespeichert sind
+
+splitdata = data.split("\",\"")         # Auslesen der ID's aus den json format
 
 
-splitdata = data.split("\",\"")
-friends = len(splitdata)/2
-zinger = len(splitdata)
+friends = len(splitdata)/2  #Anzahl der Freunde
+listAnz = len(splitdata)    #Anzahl der Elemente in der Liste
 
-newlist = []
 
-for i in range(0, zinger): # start, stop
+IDs = []
+
+for i in range(0, listAnz): # start, stop
 
     id, check = splitdata[i].split("-")
     if check is "2":
-        newlist.append(id)
-        
+        IDs.append(id)
 
-zingerNew = len(newlist)
+
+
+print(IDs)
+
+AnzIDs = len(IDs)
+
 abgleich = -1
 print(abgleich)
 
 downloadlist = []
 
-for i in range(0, zingerNew): # start, stop
+for i in range(0, AnzIDs): # start, stop
     #print("%s: %s  " % (i, newlist[i]))
-    url = "https://graph.facebook.com/" + newlist[i] + "?fields=picture.width(720).height(720)"
-    print(zingerNew - i)
+    url = "https://graph.facebook.com/" + IDs[i] + "?fields=picture.width(720).height(720)"
+    print(AnzIDs - i)
     response = urllib.urlopen(url)
     data = response.read()      # a `bytes` object
     text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
     search = "error"
-    check = text.find(search)
+    check = text.find(search)  # search the term "error"
+                                # Wenn nichts gefunden wird check=-1
     if check is abgleich:
         resp_dict = json.loads(text)
-        mitgabe = "" + resp_dict['picture']['data']['url'] + "#" + newlist[i] + ""
+        mitgabe = "" + resp_dict['picture']['data']['url'] + "#" + IDs[i] + ""
         downloadlist.append(mitgabe)
-        
-    
+
+
 
 
 
